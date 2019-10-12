88f218568f5fb41936a75d1ebce5584c4eeda9e6
pda-gb <52175155+pda-gb@users.noreply.github.com>
2019-10-13 01:12:18 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -10,23 +10,23 @@ def search_pairs(array, k):
   sub_out1 = ()
   sub_out2 = ()
   answer = []
-  while i < len(array):
+  while i < len(array):   ## перебираем каждый элемент в array
       itm = i + 1
-      a = array[i]
-      while itm < len(array):
+      a = array[i]   ## запоминаем первый элемент
+      while itm < len(array):   ## перебираем последующие...
           b = array[itm]
-          if a + b == k:
-              sub_out = (a, b)
-              out_list.append(sub_out)
+          if a + b == k:    ## что бы просуммировать с первым
+              sub_out = (a, b)   ## если их сумма равна пяти, то создаём кортеж 
+              out_list.append(sub_out)   ## и добавляем в свой список
               j = 0
               jtm = 0
-              while j < len(out_list):
+              while j < len(out_list):   ## аналогично перебираем свой список..
                   sub_out1 = out_list[j]
                   jtm = j + 1
                   while jtm < len(out_list):
                       sub_out2 = out_list[jtm]
-                      if sub_out1[0] == sub_out2[1]  or sub_out1[0] == sub_out2[0]:  ## and sub_out1[1] == sub_out2[0]:
-                          out_list.remove(sub_out2)
+                      if sub_out1[0] == sub_out2[1]  or sub_out1[0] == sub_out2[0]:     ## ...что бы найти дубли..
+                          out_list.remove(sub_out2)   ## и удалить их
                       jtm+=1
                   j+=1
           itm += 1
